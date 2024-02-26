0d1888934b3acee54f3b33d7ca77e501f44f272e
John Ahn <19668568+jtahn@users.noreply.github.com>
2024-02-26 10:05:44 -0500

    clean up
    - copied all py code into md files
    - deleted py and md folders; leetcode folder is now purely md files

--- LeetCode/py/121.py
+++
@@ -1,8 +0,0 @@
-"""dynamic programming"""
-def maxProfit(self, prices: List[int]) -> int:
-    max_profit_seen = 0 
-    lowest_price_seen = prices[0]
-    for price in prices:
-        max_profit_seen = max(max_profit_seen, price-lowest_price_seen)
-        lowest_price_seen = min(lowest_price_seen, price)
-    return max_profit_seen
